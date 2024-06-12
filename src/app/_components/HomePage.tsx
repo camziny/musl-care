414c5ee5956ca6c422b0465654354fb95902f66c
camziny <cameronziny@gmail.com>
2024-06-12 11:25:06 -0400

    - create about page - create contact page - create care seeker page - create caregiver page - add caregiver and care seeker buttons to home page - write users, careGivers, and careSeekers schema

--- src/app/_components/HomePage.tsx
+++ src/app/_components/HomePage.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
@@ -10,7 +11,27 @@ export default function HomePage() {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex flex-col items-center justify-center min-h-[50vh] bg-white py-12 px-4">
-        <h1 className="text-2xl font-bold p-4">Muslim Care</h1>
+        <h1 className="text-2xl font-bold p-4">(Name of App)</h1>
+        <h3 className="text-lg font-semibold p-4">
+          Connecting care-seekers with dedicated, background checked Muslim
+          caregivers for a harmonious home.
+        </h3>
+        <div className="flex justify-center p-4 space-x-4">
+          <div className="p-2">
+            <Link href="/care-seeker">
+              <button className="w-64 bg-stone-500 text-white rounded-lg px-6 py-3 shadow-md hover:bg-stone-400 transition-all duration-300 text-center">
+                I'm looking for care
+              </button>
+            </Link>
+          </div>
+          <div className="p-2">
+            <Link href="/care-giver">
+              <button className="w-64 bg-stone-500 text-white rounded-lg px-6 py-3 shadow-md hover:bg-stone-400 transition-all duration-300 text-center">
+                I'm a care giver
+              </button>
+            </Link>
+          </div>
+        </div>
         <div className="relative w-full max-w-md mb-8">
           <Image
             src={logoUrl}
