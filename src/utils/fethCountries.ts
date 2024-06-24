004b3a8190f23c7f2c47f70b405998da16052916
camziny <cameronziny@gmail.com>
2024-06-24 10:52:32 -0400

    - make getCountries type safe

--- src/utils/fethCountries.ts
+++ src/utils/fethCountries.ts
@@ -1,16 +1,20 @@
 export async function getCountries() {
   const res = await fetch("https://restcountries.com/v3.1/all");
-  if (!res.ok) {
-    throw new Error("Failed to fetch countries");
-  }
   const allCountriesData = await res.json();
-  const sortedCountries = allCountriesData
+
+  type Country = {
+    value: string;
+    label: string;
+    flag: string;
+  };
+
+  const sortedCountries: Country[] = allCountriesData
     .map((country: any) => ({
-      value: country.cca2,
+      value: country.name.common,
       label: country.name.common,
       flag: country.flags.svg,
     }))
-    .sort((a, b) => a.label.localeCompare(b.label));
+    .sort((a: Country, b: Country) => a.label.localeCompare(b.label));
 
   return sortedCountries;
 }
